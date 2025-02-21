dbe474c5a66c4da54bc408af58352d90dfb5f32f
ErwanDemay <erwan.demay64140@gmail.com>
2025-02-21 23:32:06 +0100

    Fonctionnel , manque technos de chaque projet et style

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,17 @@
 module.exports = function (eleventyConfig) {
+  // Copier les fichiers statiques vers "dist"
   eleventyConfig.addPassthroughCopy("src/assets/css/styles.css");
   eleventyConfig.addPassthroughCopy("src/assets/images");
-  eleventyConfig.addPassthroughCopy("src/assets/script.js"); // Ajout du script
+  eleventyConfig.addPassthroughCopy("src/assets/script.js");
+
+  // Copier les projets (y compris les images qu'ils contiennent)
+  eleventyConfig.addPassthroughCopy("src/projects");
+
+  // Créer une collection "projets" avec les fichiers index.md dans "projects"
   eleventyConfig.addCollection("projets", function (collectionApi) {
     return collectionApi.getFilteredByGlob("src/projects/**/index.md");
   });
+
   return {
     dir: {
       input: "src",
---
+++ src/projects/projet1/index.md
@@ -0,0 +1,9 @@
+---
+title: Ateliers ESAD Pyrénées
+description: Ce programme permet de naviguer dans les archives des élèves sur un serveur FTP. Il calcule et met en cache la taille et la date de modification des fichiers et dossiers pour optimiser les performances.
+image: /projects/projet1/image.png  
+github : https://github.com/esadpyrenees/archiver
+url : https://ateliers.esad-pyrenees.fr/web/archives/
+---
+
+Voici plus de détails sur ce projet...
